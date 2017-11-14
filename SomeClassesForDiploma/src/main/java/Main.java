2375f6837c110c2cb7aa91145f4fc860d54ddf2f
Дмитрий <dimaprusskii@mail.ru>
2017-11-15 01:30:33 +0300

    Insert 18 xml-files from zip to postgres and performance measurement

--- SomeClassesForDiploma/src/main/java/Main.java
+++ SomeClassesForDiploma/src/main/java/Main.java
@@ -6,15 +6,22 @@ import postgres.*;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
-
+import java.sql.SQLException;
 
 
 public class Main {
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, JSONException, TransformerException {
-        MongoDBClass mongo = new MongoDBClass();
-        mongo.insert("contracts.zip");
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, JSONException, TransformerException, SQLException {
+        //MongoDBClass mongo = new MongoDBClass();
+        //mongo.insert("contracts.zip");
+        //mongo.select();
 
-        mongo.select();
+        PostgresClass postgres = new PostgresClass();
+        //postgres.deleteTable("contracts");
+        //postgres.createTable("contracts");
+
+        //postgres.insert("contracts.zip");
+        postgres.select();
+
     }
 
 }
