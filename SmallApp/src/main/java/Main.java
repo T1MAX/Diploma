3fb380b72186ab21202da493fe7400cdf2a9bdd9
Дмитрий <dimaprusskii@mail.ru>
2017-11-19 23:19:01 +0300

    Program, that searchs xml-files, finds the right files, divide them on contracts and put it to mongoDB. (It will be application soon)

---
+++ SmallApp/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.json.JSONException;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, JSONException, TransformerException {
+        MongoDBClass mongo = new MongoDBClass();
+        mongo.insert("contracts");
+        //mongo.select();
+    }
+}
